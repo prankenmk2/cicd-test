527097aef53e060138832d60e04a03e6edb57e42
jaehyungseol <prankenmk2@gmail.com>
2025-09-26 14:18:34 +0900

    Hello World19

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -23,9 +23,9 @@ public class DemoApplication {
             InetAddress inetAddress = InetAddress.getLocalHost();
             String serverIp = inetAddress.getHostAddress();  // 서버 IP
             String hostName = inetAddress.getHostName();     // 서버 호스트명
-            return "Hello World 19 <br>- Server IP:" + serverIp + " (Host: " + hostName + ")";
+            return "Hello World 20 <br>- Server IP:" + serverIp + " (Host: " + hostName + ")";
         } catch (UnknownHostException e) {
-            return "Hello World 19 <br>- Cannot resolve server IP";
+            return "Hello World 20 <br>- Cannot resolve server IP";
         }
     }
 }
